ecaecf93dab08eabcd186d0cd3364b12721dec6c
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-23 19:18:36 +0900

    Component Creation Finish

--- src/components/App/MovieApp.js
+++ src/components/App/MovieApp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AppId, TargetDt, URL } from "../lib/index";
+import Movies from "./Movies";
 
 export default class MovieApp extends React.Component {
   state = {
@@ -10,21 +11,25 @@ export default class MovieApp extends React.Component {
   };
 
   getDailyList = async () => {
-    fetch(
-      `${URL}/searchDailyBoxOfficeList.json?key=${AppId}&targetDt=${TargetDt}`
-    )
-      .then(res => res.json())
-      .then(json => {
-        //console.log(json);
-        this.setState({
-          type: json.boxOfficeResult.boxofficeType,
-          range: json.boxOfficeResult.showRange,
-          list: json.boxOfficeResult.dailyBoxOfficeList,
-          isLoading: false
-        });
+    try {
+      fetch(
+        `${URL}/searchDailyBoxOfficeList.json?key=${AppId}&targetDt=${TargetDt}`
+      )
+        .then(res => res.json())
+        .then(json => {
+          //console.log(json);
+          this.setState({
+            type: json.boxOfficeResult.boxofficeType,
+            range: json.boxOfficeResult.showRang,
+            list: json.boxOfficeResult.dailyBoxOfficeList,
+            isLoading: false
+          });
 
-        console.log(this.state);
-      });
+          console.log(this.state);
+        });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   componentDidMount() {
@@ -32,6 +37,34 @@ export default class MovieApp extends React.Component {
   }
 
   render() {
-    return <div>Movie App</div>;
+    const { type, range, list, isLoading } = this.state;
+    return (
+      <div>
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">
+              <h2>Data Loading</h2>
+            </span>
+          </div>
+        ) : (
+          <div>
+            <div className="type">
+              <h3>{type}</h3>
+            </div>
+            <h5>Date: {range}</h5>
+            <div className="movies">
+              {list.map((list, index) => (
+                <Movies
+                  id={list.id}
+                  key={index}
+                  title={list.movieNm}
+                  Open={list.openDt}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    );
   }
 }
---
+++ src/components/App/Movies.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function lilst({ title, Open }) {
+  return (
+    <div className="movie">
+      <div className="data">
+        <a
+          href={`https://search.naver.com/search.naver?${encodeURI(
+            `sm=top_hty&fbm=1&ie=utf8&query=${title}`
+          )}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h4>영화제목 : {title}</h4>
+          <h4>개봉일자 : {Open}</h4>
+        </a>
+      </div>
+    </div>
+  );
+}
