261ca4f0d1d54eba841e560af386b926ba42e5eb
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-23 18:53:17 +0900

    modify index / Creation MovieApp

--- src/components/App/MovieApp.js
+++ src/components/App/MovieApp.js
@@ -1,6 +1,36 @@
 import React from "react";
+import { AppId, TargetDt, URL } from "../lib/index";
 
 export default class MovieApp extends React.Component {
+  state = {
+    type: "",
+    range: "",
+    list: [],
+    isLoading: true
+  };
+
+  getDailyList = async () => {
+    fetch(
+      `${URL}/searchDailyBoxOfficeList.json?key=${AppId}&targetDt=${TargetDt}`
+    )
+      .then(res => res.json())
+      .then(json => {
+        //console.log(json);
+        this.setState({
+          type: json.boxOfficeResult.boxofficeType,
+          range: json.boxOfficeResult.showRange,
+          list: json.boxOfficeResult.dailyBoxOfficeList,
+          isLoading: false
+        });
+
+        console.log(this.state);
+      });
+  };
+
+  componentDidMount() {
+    this.getDailyList();
+  }
+
   render() {
     return <div>Movie App</div>;
   }
--- src/components/lib/index.js
+++ src/components/lib/index.js
@@ -1,2 +1,3 @@
 export { default as URL } from "./URL";
 export { default as TargetDt } from "./TargetDt";
+export { default as AppId } from "./AppId/AppId";
